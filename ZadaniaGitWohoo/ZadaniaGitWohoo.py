833956bb08442820efebd3af9fd908f165e1a133
nico <student@SALE>
2024-10-17 11:13:33 +0200

    zadanie_jeden_zrobione

--- ZadaniaGitWohoo/ZadaniaGitWohoo.py
+++ ZadaniaGitWohoo/ZadaniaGitWohoo.py
@@ -1,1 +1,9 @@
+def is_critically_balanced():
+	temp = float(input("TEMPERATURE IN KELVIN: "))
+	ne = float(input("NEUTRONS PER SECOND: "))
+	if (temp < 800 or ne > 500 or temp * ne < 500000):
+		return True
+	else: 
+		return False
 
+print(is_critically_balanced())
